63fd42d8b321130e82da7a2ccc67dbd34519188b
janthony25 <junanthony.business@gmail.com>
2024-01-31 23:56:47 +1300

    Add project files.

---
+++ For-Each/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ForEachLoops
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // foreach
+            // foreach loop, which is used exclusively to loop through elements in an array:
+
+            string[] brothers = { "Wilbert", "Jun", "Limuel" };
+
+            foreach (string brother in brothers)
+            {
+                Console.WriteLine(brother);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
